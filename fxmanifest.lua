c2ca3e688704a58104feecb387249dc084b5c987
Distortionzz <132258986+Distortionzz@users.noreply.github.com>
2023-05-01 06:56:11 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,3 +4,7 @@ version "v0.1"
 description "DZ-Scammer by Distortionz_z"
 fx_version "cerulean"
 game "gta5"
+
+dependencies { 'qb-core', 'qb-progressbar', 'qb-target' } -- If you use a third party scrip, change the name. Example 'qb-progressbar' --> 'qs-progressbar'.
+
+client_scripts { 'client.lua' }
